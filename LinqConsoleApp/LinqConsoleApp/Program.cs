a33a1d1c3205056ac90491cf80089e800c963fff
RobertKurwica <itiguuy@gmail.com>
2020-05-04 11:46:30 +0200

    Init

---
+++ LinqConsoleApp/LinqConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace LinqConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var d = new LinqSamples();
+            //d.Przyklad1();
+            d.Przyklad3();
+        }
+    }
+}
